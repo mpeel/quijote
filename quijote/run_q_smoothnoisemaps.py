e3b3835a7fac43d0bc19a241d461c110b12cd8a3
Mike Peel <email@mikepeel.net>
2019-06-06 16:57:57 +0100

    +

--- quijote/run_q_smoothnoisemaps.py
+++ quijote/run_q_smoothnoisemaps.py
@@ -16,10 +16,10 @@ outdirectory = '/net/nas/proyectos/quijote/mikepeel/quijote_201905/smooth/'
 
 mfi_l,mfi_111,mfi_113,mfi_217,mfi_219,mfi_311,mfi_313,mfi_417,mfi_419 = np.loadtxt('/net/nas/proyectos/quijote/mikepeel/quijote_mfi_wfs.dat',unpack=True)
 
-prefixes = ['mfi_may2019']#,'mfi_may2019_half1','mfi_may2019_half2']
-newprefixes = ['mfi']#,'half1mfi','half2mfi']
+prefixes = ['mfi_may2019','mfi_may2019_half1','mfi_may2019_half2']
+newprefixes = ['mfi','half1mfi','half2mfi']
 newdate = '201905'
-numrealisations=100#0
+numrealisations=1000
 
 mapnumbers = [0,1,2]
 
@@ -30,4 +30,16 @@ for k in range(0,len(prefixes)):
 		for m in range(0,len(mapnumbers)):
 			# smoothnoisemap(directory, outdirectory, str(output_nside[i])+'_'+resolution+'smoothed_'+newprefixes[k]+'2_17.0_512_'+newdate+'_nhits_'+str(mapnumbers[m]), prefixes[k]+'_nhits_17.0_2.fits',mapnumber=mapnumbers[m],numrealisations=numrealisations,fwhm=output_resolution[i],sigma_0=1.0*np.sqrt(25),nside=output_nside,windowfunction=np.sqrt(mfi_217),usehealpixfits=True)
 
+			smoothnoisemap(directory, outdirectory, str(output_nside[i])+'_'+resolution+'smoothed_'+newprefixes[k]+'2_17.0_512_'+newdate+'_nhits_'+str(mapnumbers[m]), prefixes[k]+'_nhits_17.0_2.fits',mapnumber=mapnumbers[m],numrealisations=numrealisations,fwhm=output_resolution[i],sigma_0=1.0*np.sqrt(25),nside=output_nside,windowfunction=np.sqrt(mfi_217),usehealpixfits=True)
+
 			smoothnoisemap(directory, outdirectory, str(output_nside[i])+'_'+resolution+'smoothed_'+newprefixes[k]+'2_17.0_512_'+newdate+'_weight_'+str(mapnumbers[m]), prefixes[k]+'_weights_17.0_2.fits',mapnumber=mapnumbers[m],numrealisations=numrealisations,fwhm=output_resolution[i],nside=output_nside,windowfunction=np.sqrt(mfi_217),usehealpixfits=True)
+
+			smoothnoisemap(directory, outdirectory, str(output_nside[i])+'_'+resolution+'smoothed_'+newprefixes[k]+'2_19.0_512_'+newdate+'_weight_'+str(mapnumbers[m]), prefixes[k]+'_weights_19.0_2.fits',mapnumber=mapnumbers[m],numrealisations=numrealisations,fwhm=output_resolution[i],nside=output_nside,windowfunction=np.sqrt(mfi_219),usehealpixfits=True)
+
+			smoothnoisemap(directory, outdirectory, str(output_nside[i])+'_'+resolution+'smoothed_'+newprefixes[k]+'3_11.0_512_'+newdate+'_weight_'+str(mapnumbers[m]), prefixes[k]+'_weights_11.0_3.fits',mapnumber=mapnumbers[m],numrealisations=numrealisations,fwhm=output_resolution[i],nside=output_nside,windowfunction=np.sqrt(mfi_311),usehealpixfits=True)
+
+			smoothnoisemap(directory, outdirectory, str(output_nside[i])+'_'+resolution+'smoothed_'+newprefixes[k]+'3_13.0_512_'+newdate+'_weight_'+str(mapnumbers[m]), prefixes[k]+'_weights_13.0_3.fits',mapnumber=mapnumbers[m],numrealisations=numrealisations,fwhm=output_resolution[i],nside=output_nside,windowfunction=np.sqrt(mfi_313),usehealpixfits=True)
+
+			smoothnoisemap(directory, outdirectory, str(output_nside[i])+'_'+resolution+'smoothed_'+newprefixes[k]+'4_17.0_512_'+newdate+'_weight_'+str(mapnumbers[m]), prefixes[k]+'_weights_17.0_4.fits',mapnumber=mapnumbers[m],numrealisations=numrealisations,fwhm=output_resolution[i],nside=output_nside,windowfunction=np.sqrt(mfi_417),usehealpixfits=True)
+
+			smoothnoisemap(directory, outdirectory, str(output_nside[i])+'_'+resolution+'smoothed_'+newprefixes[k]+'4_19.0_512_'+newdate+'_weight_'+str(mapnumbers[m]), prefixes[k]+'_weights_19.0_4.fits',mapnumber=mapnumbers[m],numrealisations=numrealisations,fwhm=output_resolution[i],nside=output_nside,windowfunction=np.sqrt(mfi_417),usehealpixfits=True)
