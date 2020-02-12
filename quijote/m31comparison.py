3cfa3c7f93ec183227d279da425d0e28b8ae9763
Mike Peel <email@mikepeel.net>
2020-02-12 17:27:09 +0000

    +

--- quijote/m31comparison.py
+++ quijote/m31comparison.py
@@ -21,8 +21,8 @@ map_prefix = '/Users/mpeel/Documents/maps/wmap_planck_smoothed_maps_2015/'
 filenames = ['256_60.00smoothed_wmap9decbeamCMBSmicasub_22.8_512_2013_mKCMBunits.fits','256_60.00smoothed_wmap9decbeamCMBSmicasub_33.0_512_2013_mKCMBunits.fits','256_60.00smoothed_wmap9decbeamCMBSmicasub_40.7_512_2013_mKCMBunits.fits','256_60.00smoothed_wmap9decbeamCMBSmicasub_60.7_512_2013_mKCMBunits.fits','256_60.00smoothed_wmap9decbeamCMBSmicasub_93.5_512_2013_mKCMBunits.fits','256_60.00smoothed_PlanckR2fullbeamCMBSmicasub_28.4_1024_2015_mKCMBunits.fits','256_60.00smoothed_PlanckR2fullbeamCMBSmicasub_44.1_1024_2015_mKCMBunits.fits','256_60.00smoothed_PlanckR2fullbeamCMBSmicasub_70.4_1024_2015_mKCMBunits.fits','256_60.00smoothed_PlanckR2fullbeamCMBSmicasub_100_2048_2015_mKCMBunits.fits','256_60.00smoothed_PlanckR2fullbeamCMBSmicasub_143_2048_2015_mKCMBunits.fits','256_60.00smoothed_PlanckR2fullbeamCMBSmicasub_217_2048_2015_mKCMBunits.fits','256_60.00smoothed_PlanckR2fullbeamCMBSmicasub_353_2048_2015_mKCMBunits.fits','../'+qfolder+'/reform/mfi_'+qext+'_map_11.0_1.fits','../'+qfolder+'/reform/mfi_'+qext+'_map_13.0_1.fits','../'+qfolder+'/reform/mfi_'+qext+'_map_17.0_2.fits','../'+qfolder+'/reform/mfi_'+qext+'_map_19.0_2.fits','../'+qfolder+'/reform/mfi_'+qext+'_map_11.0_3.fits','../'+qfolder+'/reform/mfi_'+qext+'_map_11.0_3.fits','../'+qfolder+'/reform/mfi_'+qext+'_map_17.0_4.fits','../'+qfolder+'/reform/mfi_'+qext+'_map_19.0_4.fits']
 freqs = [22.8,33.0,40.7,60.7,93.5,28.4,44.1,70.4,100.0,143.0,217.0,353.0,11.0,13.0,17.0,19.0,11.0,13.0,17.0,19.0]
 # quijote_map = '/Users/mpeel/Documents/maps/quijote_201907/quijote_combinedmaps_select_withcov_rfi_jul2019_withcov_rfi_nside512_finalmaskfdec2.fits'
-quijote_map = '/Users/mpeel/Documents/QUIJOTE/m31/mapsds_iqu_m31_comb_rasters_931td_rgs_nominal_oct2018_ctod_ns512.fits'
-# quijote_map = '/Users/mpeel/Documents/QUIJOTE/m31/quijote_combinedmaps_nominal_and_m31_test_rfi_newflags2_may2019_withper5_test_rfi_newflags2_nside512.fits'
+# quijote_map = '/Users/mpeel/Documents/QUIJOTE/m31/mapsds_iqu_m31_comb_rasters_931td_rgs_nominal_oct2018_ctod_ns512.fits'
+quijote_map = '/Users/mpeel/Documents/QUIJOTE/m31/quijote_combinedmaps_nominal_and_m31_test_rfi_newflags2_may2019_withper5_test_rfi_newflags2_nside512.fits'
 
 outdir = '/Users/mpeel/Documents/QUIJOTE/m31/comparison_zoom_202001/'
 
@@ -97,40 +97,40 @@ print(np.shape(data))
 print(len(data[1]))
 print(len(data[2][0]))
 
-for i in range(0,len(data[1])):
-	# print(data[1][i])
-	# print(data[2][0][i])
-	# print(len(data[2][0][i]))
-	# maps[0][data[1][i]] = data[2][0][i][0][0]
-	# maps[1][data[1][i]] = data[2][0][i][0][1]
-	# maps[2][data[1][i]] = data[2][0][i][0][2]
-	# maps[3][data[1][i]] = data[2][0][i][0][3]
-	# maps[4][data[1][i]] = data[2][0][i][1][0]
-	# maps[5][data[1][i]] = data[2][0][i][1][1]
-	# maps[6][data[1][i]] = data[2][0][i][1][2]
-	# maps[7][data[1][i]] = data[2][0][i][1][3]
-	for k in range(0,2): # Low and high
-		for l in range(0,4): # Horns
-			# maps[k*4+l][0][data[1][i]] = data[0][0][k][l][:]
-			# maps[k*4+l][1][data[1][i]] = data[0][1][k][l][:]
-			# maps[k*4+l][2][data[1][i]] = data[0][2][k][l][:]
-			# These were for the smoothed maps
-			maps[k*4+l][0][data[1][i]] = data[2][0][i][k][l]
-			maps[k*4+l][1][data[1][i]] = data[2][1][i][k][l]
-			maps[k*4+l][2][data[1][i]] = data[2][2][i][k][l]
+# for i in range(0,len(data[1])):
+# 	# print(data[1][i])
+# 	# print(data[2][0][i])
+# 	# print(len(data[2][0][i]))
+# 	# maps[0][data[1][i]] = data[2][0][i][0][0]
+# 	# maps[1][data[1][i]] = data[2][0][i][0][1]
+# 	# maps[2][data[1][i]] = data[2][0][i][0][2]
+# 	# maps[3][data[1][i]] = data[2][0][i][0][3]
+# 	# maps[4][data[1][i]] = data[2][0][i][1][0]
+# 	# maps[5][data[1][i]] = data[2][0][i][1][1]
+# 	# maps[6][data[1][i]] = data[2][0][i][1][2]
+# 	# maps[7][data[1][i]] = data[2][0][i][1][3]
+# 	for k in range(0,2): # Low and high
+# 		for l in range(0,4): # Horns
+# 			# maps[k*4+l][0][data[1][i]] = data[0][0][k][l][:]
+# 			# maps[k*4+l][1][data[1][i]] = data[0][1][k][l][:]
+# 			# maps[k*4+l][2][data[1][i]] = data[0][2][k][l][:]
+# 			# These were for the smoothed maps
+# 			maps[k*4+l][0][data[1][i]] = data[2][0][i][k][l]
+# 			maps[k*4+l][1][data[1][i]] = data[2][1][i][k][l]
+# 			maps[k*4+l][2][data[1][i]] = data[2][2][i][k][l]
 
-# for k in range(0,2): # Low and high
-# 	for l in range(0,4): # Horns
-# 		maps[k*4+l][0] = data[0][0][k][l]
-# 		maps[k*4+l][1] = data[0][1][k][l]
-# 		maps[k*4+l][2] = data[0][2][k][l]
+for k in range(0,2): # Low and high
+	for l in range(0,4): # Horns
+		maps[k*4+l][0] = data[0][0][k][l]
+		maps[k*4+l][1] = data[0][1][k][l]
+		maps[k*4+l][2] = data[0][2][k][l]
 
 print(np.shape(maps))
 for i in range(0,nummaps):
 	for j in range(0,2): # Pol
 
-		newmap = hp.ud_grade(maps[i][j],256,order_in='NEST',order_out='RING')
-		# newmap = hp.ud_grade(maps[i][j],256,order_in='RING',order_out='RING')
+		# newmap = hp.ud_grade(maps[i][j],256,order_in='NEST',order_out='RING')
+		newmap = hp.ud_grade(maps[i][j],256,order_in='RING',order_out='RING')
 		hp.write_map(outdir+'mfi_'+str(i+1)+'_'+str(j+1)+'.fits',newmap,overwrite=True)
 		hp.mollview(newmap)
 		plt.savefig(outdir+'mfi_'+str(i+1)+'_'+str(j+1)+'.pdf')
